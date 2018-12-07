95300eb6e79c2e916bca5dcb504d897302701c45
Paloma Ania <paloma.aniabenito@gmail.com>
2018-12-07 17:13:37 +0100

    SPLIT the name and give styles to it

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,23 +5,26 @@ const button = document.querySelector('#btn');
 
 function handledSearchUser() {
     const username = nameUserInput.value;
-    fetch(`https://api.github.com/users/${username}`) 
-    .then(response => response.json())
-    .then(data => {
-        const name = document.querySelector('.user_name');
-        name.innerHTML = data.name;
-  });
-        
-};
+    fetch(`https://api.github.com/users/${username}`)
+        .then(response => response.json())
+        .then(data => {
+            const name = document.querySelector('.user_name');
+            name.innerHTML = data.name;
 
+            let formattedName = data.name.split(' ')[0];
+            let splittedName = formattedName.trim().split('');
+
+            let letterList = document.body.querySelector('.letter-list');
+            let listItems = '';
+            for (let letter of splittedName) {
+                listItems += `<li class="li">${letter}</li>`;
+            }
+            letterList.innerHTML = listItems;
+
+            console.log(formattedName)
+        });
+
+}
 
 button.addEventListener('click', handledSearchUser);
 
-
-/*
-let sentence = document.querySelector('.user_name');
-sentence.innerHTML = divideCharacters () {
-    sentence.split(" ");
-    let pieces = sentence.split("");
-}
-*/ 
